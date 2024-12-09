fc3d58235f228d275ad0ada347370fc9667460b6
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-12-09 10:46:26 -0300

    feat: add resume components

---
+++ src/components/pages/dashboard/resume/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { InfosSidebar } from "./infos-sidebar";
+import { ResumeContent } from "./resume-content";
+import { StructureSidebar } from "./structure-sidebar";
+import { FormProvider, useForm } from "react-hook-form";
+
+export const ResumePage = () => {
+  const defaultValues: ResumeData = {
+    content: {
+      image: {
+        url: "",
+        visible: true,
+      },
+      infos: {
+        email: "",
+        fullName: "",
+        headline: "",
+        location: "",
+        phone: "",
+        website: "",
+      },
+      summary: "",
+      certifications: [],
+      educations: [],
+      experiences: [],
+      languages: [],
+      projects: [],
+      skills: [],
+      socialMedias: [],
+    },
+  };
+  const methods = useForm<ResumeData>({ defaultValues });
+
+  return (
+    <FormProvider {...methods}>
+      <main className="w-full h-screen overflow-hidden">
+        <ResizablePanelGroup direction="horizontal" className="w-full h-full">
+          <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>
+            <InfosSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+
+          <ResizablePanel>
+            <ResumeContent />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+
+          <ResizablePanel minSize={20} maxSize={35} defaultSize={25}>
+            <StructureSidebar />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
+    </FormProvider>
+  );
+};
---
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Logo from "@/assets/logo.svg";
+import { Separator } from "@/components/ui/separator";
+import { AIGenerationDropdown } from "./ai-generation-dropdown";
+import { BasicInfoSection } from "./sections/basic-info";
+import { SummarySection } from "./sections/summary";
+import { MultiplesSections } from "./sections/multiples";
+
+export const InfosSidebar = () => {
+  return (
+    <aside className="w-full h-full p-6 overflow-y-auto">
+      <div className="w-full flex items-center justify-between">
+        <Link href="/dashboard/resumes">
+          <Logo className="w-full max-w-[80px]" />
+        </Link>
+
+        <AIGenerationDropdown />
+      </div>
+      <Separator className="my-5" />
+
+      <BasicInfoSection />
+      <Separator className="my-5" />
+
+      <SummarySection />
+      <Separator className="my-5" />
+
+      <MultiplesSections />
+    </aside>
+  );
+};

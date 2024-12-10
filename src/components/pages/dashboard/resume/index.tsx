a05af3dfb9cbfadb25fa7865686534075bf9edae
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-12-10 15:12:00 -0300

    feat: saving form data in db

--- src/components/pages/dashboard/resume/index.tsx
+++ src/components/pages/dashboard/resume/index.tsx
@@ -9,23 +9,38 @@ import { InfosSidebar } from "./infos-sidebar";
 import { ResumeContent } from "./resume-content";
 import { StructureSidebar } from "./structure-sidebar";
 import { FormProvider, useForm } from "react-hook-form";
+import { User } from "next-auth";
+import { useCallback, useEffect, useRef } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
+import { updateResumeData } from "@/db/actions";
+import { useParams } from "next/navigation";
+import { mergician } from "mergician";
 
-export const ResumePage = () => {
+type ResumePageProps = {
+  title: string;
+  initialData: Partial<ResumeData>;
+  user?: User;
+};
+
+export const ResumePage = ({ title, initialData, user }: ResumePageProps) => {
+  const params = useParams();
+  const resumeId = params?.id as string;
+
   const defaultValues: ResumeData = {
     content: {
+      summary: "<p></p>",
       image: {
-        url: "",
+        url: user?.image ?? "",
         visible: true,
       },
       infos: {
-        email: "",
-        fullName: "",
+        email: user?.email ?? "",
+        fullName: user?.name ?? "",
         headline: "",
         location: "",
         phone: "",
         website: "",
       },
-      summary: "",
       certifications: [],
       educations: [],
       experiences: [],
@@ -70,7 +85,31 @@ export const ResumePage = () => {
       },
     },
   };
-  const methods = useForm<ResumeData>({ defaultValues });
+  const methods = useForm<ResumeData>({
+    defaultValues: mergician(defaultValues, initialData),
+  });
+
+  const data = methods.watch();
+  const debouncedData = useDebounce(JSON.stringify(data));
+
+  const shouldSave = useRef(false);
+
+  const handleSaveUpdates = useCallback(async () => {
+    try {
+      if (!shouldSave.current) {
+        shouldSave.current = true;
+        return;
+      }
+      const updatedData = methods.getValues();
+      await updateResumeData(resumeId, updatedData);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [methods]);
+
+  useEffect(() => {
+    handleSaveUpdates();
+  }, [debouncedData, handleSaveUpdates]);
 
   return (
     <FormProvider {...methods}>
@@ -82,7 +121,7 @@ export const ResumePage = () => {
           <ResizableHandle withHandle />
 
           <ResizablePanel>
-            <ResumeContent />
+            <ResumeContent title={title}/>
           </ResizablePanel>
           <ResizableHandle withHandle />
 
--- src/components/ui/editor/index.tsx
+++ src/components/ui/editor/index.tsx
@@ -6,6 +6,7 @@ import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import { cn } from "@/lib/utils";
 import { MenuBar } from "./menu-bar";
+import { useEffect } from "react";
 
 type EditorProps = {
   value: string;
@@ -19,14 +20,14 @@ export const Editor = ({ value, onChange, className }: EditorProps) => {
       StarterKit.configure({
         bulletList: {
           HTMLAttributes: {
-            class: "list-disc pl-4",
-          },
+            class: "list-disc pl-4"
+          }
         },
         orderedList: {
           HTMLAttributes: {
-            class: "list-decimal pl-4",
-          },
-        },
+            class: "list-decimal pl-4"
+          }
+        }
       }),
       Underline,
       TextAlign.configure({
@@ -36,14 +37,27 @@ export const Editor = ({ value, onChange, className }: EditorProps) => {
     content: value,
     editorProps: {
       attributes: {
-        class: "focus:outline-none h-full p-4",
-      },
+        class: "focus:outline-none h-full p-4"
+      }
     },
     onCreate({ editor }) {
       onChange?.(editor.getHTML());
     },
+    onUpdate({ editor }) {
+      onChange?.(editor.getHTML());
+    },
     autofocus: false,
   });
+
+  useEffect(() => {
+    const editorHTML = editor?.getHTML();
+
+    if (editorHTML !== value) {
+      setTimeout(() => {
+        editor?.commands.setContent(value);
+      }, 0);
+    }
+  }, [value])
 
   return (
     <div

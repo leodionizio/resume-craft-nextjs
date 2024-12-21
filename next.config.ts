4e22aa47ba54942eaaf1cf90665606cb0016c9a6
Leonardo Dionizio <leonardo.dionizio@kovi.com.br>
2024-12-21 16:57:11 -0300

    chore: fix external packages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg")
